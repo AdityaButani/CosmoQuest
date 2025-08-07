2d100b7da41cd8018134a935193f3409b0ca51b0
Aditya <adityabutani29@gmail.com>
2025-08-08 03:51:30 +0530

    Fix

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ from dotenv import load_dotenv
 
 # Load environment variables
 load_dotenv()
-from dotenv import load_dotenv
-load_dotenv()
 
 # Configure logging
 logging.basicConfig(level=logging.DEBUG)
